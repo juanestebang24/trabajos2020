08e4e8629faad6e4318dbabfd31abde89ffdcb1a
juanestebang24 <66138435+juanestebang24@users.noreply.github.com>
2020-07-02 13:19:58 -0500

    Add files via upload

---
+++ 7 ejercicie/fullbody.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const one=document.querySelector('.one');
+one.innerHTML= '1'
+const two =document.querySelector('.two');
+two.innerHTML='2'
+const three=document.querySelector('.three');
+three.innerHTML='3'
