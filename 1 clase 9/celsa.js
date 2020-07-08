2ca96d3a0962f96de1c115510df7c88dfb6416ff
juanestebang24 <66138435+juanestebang24@users.noreply.github.com>
2020-07-08 14:27:25 -0500

    Add files via upload
    clase 9

---
+++ 1 clase 9/celsa.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const multiplicacion = (num1,num2) => {
+
+    return num1*num2;
+
+}
+
+
+console.log(multiplicacion(4,8));
+console.log(multiplicacion(15,3));
+console.log(multiplicacion(98,2));
